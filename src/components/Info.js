15fd0b608405d705a9353bab047b48391c62ff49
ABASITJAMALBROTHERS <noman.rauf@jamalbrothers.com.pk>
2022-10-31 17:29:41 +0500

    create BusinessCard

---
+++ src/components/Info.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from '../image/pic.jpg'
+import '../style.css'
+
+export default function Info() {
+  return (
+    <div>
+      <img src={Logo} alt="img" />
+      <h1 className="h1">Noman Memon</h1>
+      <links className="links">
+        <button className="glow-on-hover">Email</button>
+        <button className="glow-on-hover">LinkdIn</button>
+      </links>
+    </div>
+  )
+}
---
+++ src/components/Interest.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../style.css'
+
+export default function Interest() {
+  return (
+    <div>
+      <h3 className="interest_h3">Interest</h3>
+      <p className="interest">
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
+  )
+}
